88456d48982dd846e4969278c51808ab8d48aa89
Yakob Dereje <yakobdereje.yd@gmail.com>
2025-11-20 10:06:28 -0800

    on progress home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+import Image from "next/image";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+          <h2 className="text-primary-100"> Skill Path</h2>
+        </Link>
+      </nav>
+
+      {children}
+    </div>
+  );
 };
 
 export default RootLayout;
